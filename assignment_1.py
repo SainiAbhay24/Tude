5ce19c3c307dce47af81c45ec66b0c1665cd06da
Abhay Saini <sainiabhay835@gmail.com>
2025-10-11 18:28:18 +0530

    Add files via upload

---
+++ assignment_1.py
@@ -0,0 +1,32 @@
+''' 
+Task 1: Perform Basic Mathematical Operations
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division
+3.  Displays the results of each operation on the screen.
+
+'''
+
+num1 = float(input("Enter your First number: "))
+num2 = float(input("Enter your First number: "))
+
+print("Addition = " ,num1 + num2)
+print("Subtraction = ", num1 - num2)
+print("Multiplication = ", num1 * num2)
+print("Division = ", num1 - num2)
+
+'''
+Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+
+'''
+first = input("Enter your first name: ")
+last = input("Enter your last name: ")
+
+print("Hello,", first , last, "Welcome to the Python Program.")
